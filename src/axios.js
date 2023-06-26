b856219e07d8fdb8c59167f799dd4d66a22cc4ca
m4tiss <mateusz039@op.pl>
2023-06-26 11:09:54 +0200

    URL correct v4

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: process.env.API_URL || 'https://notateczki-backend.onrender.com'
+    baseURL: process.env.API_URL || 'https://notateczki-backend.onrender.com/api'
 });
  
 export default instance;
